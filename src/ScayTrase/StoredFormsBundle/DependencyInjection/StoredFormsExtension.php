33cdf761863a43cd01479cecda838263614f7102
Pavel Batanov <pavel@batanov.me>
2015-05-26 16:13:12 +0300

    Fixed config name

--- src/ScayTrase/StoredFormsBundle/DependencyInjection/StoredFormsExtension.php
+++ src/ScayTrase/StoredFormsBundle/DependencyInjection/StoredFormsExtension.php
@@ -21,7 +21,7 @@ class StoredFormsExtension extends Extension implements PrependExtensionInterfac
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('form_types.yml');
+        $loader->load('field_types.yml');
         $loader->load('services.yml');
     }
 
