988c931dd36929706fd70c0c1ae0184b47d236f5
Pavel Batanov <pavel@batanov.me>
2015-05-29 14:24:11 +0300

    required, multiple, extended fields at configuration form

--- src/ScayTrase/StoredFormsBundle/Form/Type/AbstractFieldType.php
+++ src/ScayTrase/StoredFormsBundle/Form/Type/AbstractFieldType.php
@@ -31,6 +31,7 @@ class AbstractFieldType extends AbstractType
     {
         $builder->add('name', 'text', array('label' => 'Код поля'));
         $builder->add('title', 'text', array('label' => 'Описание'));
+        $builder->add('required', 'checkbox', array('required' => false));
         $builder->add('help_message', 'textarea', array('label' => 'Подсказка', 'required' => false));
     }
 
