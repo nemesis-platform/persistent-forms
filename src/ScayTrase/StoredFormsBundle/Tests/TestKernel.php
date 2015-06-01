8564eb2216f7c8cf92aba468c5a5d47947ce35dc
Pavel Batanov <pavel@batanov.me>
2015-06-01 12:51:14 +0300

    persistence testing

---
+++ src/ScayTrase/StoredFormsBundle/Tests/TestKernel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel Batanov <pavel@batanov.me>
+ * Date: 01.06.2015
+ * Time: 10:56
+ */
+
+namespace ScayTrase\StoredFormsBundle\Tests;
+
+use ScayTrase\Testing\KernelForTest;
+
+class TestKernel extends KernelForTest
+{
+
+}
